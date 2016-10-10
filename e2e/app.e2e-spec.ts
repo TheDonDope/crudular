664d60dd02eb8c5b5f9cebbadccd756c0a23fda4
angular-cli <angular-cli@angular.io>
2016-10-10 18:19:41 +0200

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { CrudularPage } from './app.po';
+
+describe('crudular App', function() {
+  let page: CrudularPage;
+
+  beforeEach(() => {
+    page = new CrudularPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('cru works!');
+  });
+});
