d6f9f3a118cc0cec90ac9eb3e74b71dfc6e31470
TheDonDope <idontreallyneedthese@gmail.com>
2016-11-27 09:58:25 +0100

    docs(misc): update documentation
    Update the inline documentation of the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { LoginModule } from './login/login.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 /**
- * The AppModule provides the root module of the application. It imports all existing feature modules.
+ * The AppModule provides the root module of the application. It imports all necessary @angular modules as well as the
+ * application specific modules `CoreModule`, `SharedModule` and all feature modules.
  */
 @NgModule({
   declarations: [AppComponent],
@@ -23,7 +23,7 @@ import { SharedModule } from './shared/shared.module';
     FormsModule,
     // @angular/http
     HttpModule,
-    // app.routing
+    // app-routing.module
     AppRoutingModule,
     // core/core.module
     CoreModule,
