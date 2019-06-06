02764b2846f7d66eb9882a2d146ef8d0d4e8a8c1
TheDonDope <idontreallyneedthese@gmail.com>
2019-06-06 22:30:34 +0200

    feat: update to nx workspace 8 and angular 8

---
+++ apps/crudular/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'http://78.46.194.40:4321/'
-};
