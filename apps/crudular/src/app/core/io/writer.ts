9d98dc4fbe765e62575dbda7e5f11557f3630e24
TheDonDope <thedondope@hey.com>
2022-07-26 23:47:36 +0200

    feat(core): add first iteration of logging service

---
+++ apps/crudular/src/app/core/io/writer.ts
@@ -0,0 +1,12 @@
+// Writer is the interface that wraps the basic Write method.
+//
+// Write writes len(p) bytes from p to the underlying data stream.
+// It returns the number of bytes written from p (0 <= n <= len(p))
+// and any error encountered that caused the write to stop early.
+// Write must return a non-nil error if it returns n < len(p).
+// Write must not modify the slice data, even temporarily.
+//
+// Implementations must not retain p.
+export interface Writer {
+  write(p: Uint8Array): [n: number, err: Error | null];
+}
--- apps/crudular/src/app/core/logging.service.ts
+++ apps/crudular/src/app/core/logging.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class LoggingService {
+import { Writer } from './io/writer';
 
-  constructor() { }
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggingService implements Writer {
+  write(p: Uint8Array): [n: number, err: Error | null] {
+    this.log(p.toString());
+    const n = p.length;
+    const err = null;
+    return [n, err];
+  }
+  log(msg: string): void {
+    console.log(msg);
+  }
+
+  error(msg: string): void {
+    console.error(msg);
+  }
+
+  warn(msg: string): void {
+    console.warn(msg);
+  }
 }
