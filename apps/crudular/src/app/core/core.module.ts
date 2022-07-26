29ac8e43fc71d6dce565f084b92373469ddc9de0
TheDonDope <thedondope@hey.com>
2022-07-26 23:30:05 +0200

    feat(core/navigation): generate navigation module
    - run $ nx g m core/navigation --module core

--- apps/crudular/src/app/core/core.module.ts
+++ apps/crudular/src/app/core/core.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { AuthenticationModule } from './authentication/authentication.module';
+import { NavigationModule } from './navigation/navigation.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, AuthenticationModule],
+  imports: [CommonModule, AuthenticationModule, NavigationModule],
 })
 export class CoreModule {}
