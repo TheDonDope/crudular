fe93af6c5dea10f0ba7195f8f776a9555108264d
TheDonDope <thedondope@hey.com>
2022-07-26 23:56:29 +0200

    test(core): add logging service tests

--- apps/crudular/src/app/core/logging.service.spec.ts
+++ apps/crudular/src/app/core/logging.service.spec.ts
@@ -13,4 +13,23 @@ describe('LoggingService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should log', () => {
+    service.log('test');
+  });
+
+  it('should warn', () => {
+    service.warn('test');
+  });
+
+  it('should error', () => {
+    service.error('test');
+  });
+
+  it('should write', () => {
+    const p = new Uint8Array(0);
+    const [n, err] = service.write(p);
+    expect(n).toBe(0);
+    expect(err).toBeNull();
+  });
 });
